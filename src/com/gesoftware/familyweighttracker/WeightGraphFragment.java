1cdedfe8aa9b2e8ff3b8408599aa76c94987774a
gevely <gary.evely@gmail.com>
2012-11-09 21:13:56 +0000

    Add graph library
    Update 1

--- src/com/gesoftware/familyweighttracker/WeightGraphFragment.java
+++ src/com/gesoftware/familyweighttracker/WeightGraphFragment.java
@@ -1,16 +1,21 @@
 package com.gesoftware.familyweighttracker;
 
-import com.gesoftware.familyweighttracker.data.Weight;
-import com.gesoftware.familyweighttracker.data.WeightsDataSource;
-import com.gesoftware.familyweighttracker.R;
-
-import android.os.Bundle;
 import android.app.Fragment;
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.gesoftware.familyweighttracker.data.Weight;
+import com.gesoftware.familyweighttracker.data.WeightsDataSource;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
 
 public class WeightGraphFragment extends Fragment {
 
@@ -34,7 +39,25 @@ public class WeightGraphFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.graph_fragment, container, false);
+		View layout = inflater.inflate(R.layout.weight_graph_fragment, container, false);
+		
+		// init example series data  
+		GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {  
+		      new GraphViewData(1, 2.0d)  
+		      , new GraphViewData(2, 1.5d)  
+		      , new GraphViewData(3, 2.5d)  
+		      , new GraphViewData(4, 1.0d)  
+		});  
+		  
+		GraphView graphView = new LineGraphView(appContext, "GraphViewDemo");  
+		graphView.addSeries(exampleSeries); // data  
+		  
+		FrameLayout graphLayout = (FrameLayout) layout.findViewById(R.id.graphFrame);  
+		graphLayout.addView(graphView); 
+		
+		return layout;
+		
+		
 	}
 
 }
